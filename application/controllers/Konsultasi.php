3abf4e285840a523b47666e28bc0b2c9215f6584
atikaanis <atikanis9@gmail.com>
2016-01-31 14:31:40 +0700

    edit mb md

--- application/controllers/Konsultasi.php
+++ application/controllers/Konsultasi.php
@@ -40,25 +40,25 @@ class Konsultasi extends CI_Controller{
             $hasil_mb['tidak'] = $data_yes[$key]['tidak']['mb'] + $data_yes[$key+1]['tidak']['mb'] * (1 - $hipotesis_tidak_mb);
 
             $hipotesis_ringan_mb = ($key == 0) ? $data_yes[$key+1]['ringan']['mb'] : $hasil_mb['ringan'];
-            $hasil_mb['ringan'] = $data_yes[$key]['ringan']['mb'] + $data_yes[$key+1]['ringan']['mb'] * (1 - $hipotesis);
+            $hasil_mb['ringan'] = $data_yes[$key]['ringan']['mb'] + $data_yes[$key+1]['ringan']['mb'] * (1 - $hipotesis_ringan_mb);
 
             $hipotesis_sedang_mb = ($key == 0) ? $data_yes[$key+1]['sedang']['mb'] : $hasil_mb['sedang'];
-            $hasil_mb['sedang'] = $data_yes[$key]['sedang']['mb'] + $data_yes[$key+1]['sedang']['mb'] * (1 - $hipotesis);
+            $hasil_mb['sedang'] = $data_yes[$key]['sedang']['mb'] + $data_yes[$key+1]['sedang']['mb'] * (1 - $hipotesis_sedang_mb);
 
             $hipotesis_berat = ($key == 0) ? $data_yes[$key+1]['berat']['mb'] : $hasil_mb['berat'];
-            $hasil_mb['berat'] = $data_yes[$key]['berat']['mb'] + $data_yes[$key+1]['berat']['mb'] * (1 - $hipotesis);
+            $hasil_mb['berat'] = $data_yes[$key]['berat']['mb'] + $data_yes[$key+1]['berat']['mb'] * (1 - $hipotesis_berat_mb);
 
-            $hipotesis_tidak = ($key == 0) ? $data_yes[$key+1]['tidak']['mb'] : $hasil_mb['tidak'];
-            $hasil_mb['tidak'] = $data_yes[$key]['tidak']['mb'] + $data_yes[$key+1]['tidak']['mb'] * (1 - $hipotesis);
+            $hipotesis_tidak = ($key == 0) ? $data_yes[$key+1]['tidak']['md'] : $hasil_md['tidak'];
+            $hasil_md['tidak'] = $data_yes[$key]['tidak']['md'] + $data_yes[$key+1]['tidak']['md'] * (1 - $hipotesis_tidak_md);
 
-            $hipotesis_ringan = ($key == 0) ? $data_yes[$key+1]['ringan']['mb'] : $hasil_mb['ringan'];
-            $hasil_mb['ringan'] = $data_yes[$key]['ringan']['mb'] + $data_yes[$key+1]['ringan']['mb'] * (1 - $hipotesis);
+            $hipotesis_ringan = ($key == 0) ? $data_yes[$key+1]['ringan']['md'] : $hasil_md['ringan'];
+            $hasil_md['ringan'] = $data_yes[$key]['ringan']['md'] + $data_yes[$key+1]['ringan']['md'] * (1 - $hipotesis_ringan_md);
 
-            $hipotesis_sedang = ($key == 0) ? $data_yes[$key+1]['sedang']['mb'] : $hasil_mb['sedang'];
-            $hasil_mb['sedang'] = $data_yes[$key]['sedang']['mb'] + $data_yes[$key+1]['sedang']['mb'] * (1 - $hipotesis);
+            $hipotesis_sedang = ($key == 0) ? $data_yes[$key+1]['sedang']['md'] : $hasil_md['sedang'];
+            $hasil_md['sedang'] = $data_yes[$key]['sedang']['md'] + $data_yes[$key+1]['sedang']['md'] * (1 - $hipotesis_sedang_md);
 
-            $hipotesis_berat = ($key == 0) ? $data_yes[$key+1]['berat']['mb'] : $hasil_mb['berat'];
-            $hasil_mb['berat'] = $data_yes[$key]['berat']['mb'] + $data_yes[$key+1]['berat']['mb'] * (1 - $hipotesis);
+            $hipotesis_berat = ($key == 0) ? $data_yes[$key+1]['berat']['md'] : $hasil_md['berat'];
+            $hasil_md['berat'] = $data_yes[$key]['berat']['md'] + $data_yes[$key+1]['berat']['md'] * (1 - $hipotesis_berat_md);
         }
 
         print_r($data_reguler);
